6ddd65477a2ef67d7c10be327d5089398d3bfee5
Jared Forsyth <jared@jaredforsyth.com>
2023-03-10 08:41:02 -0600

    [track-path] ok we dont have backspace, but the other things are looking pretty solid

--- web/ByHand.tsx
+++ web/ByHand.tsx
@@ -5,55 +5,78 @@ import { fromMCST, ListLikeContents } from '../src/types/mcst';
 import { getKeyUpdate } from './mods/getKeyUpdate';
 import { selectEnd } from './mods/navigate';
 
-const text =
+const initialText =
     '(fn [one:two three:(four five)]:six {10 20 yes "ok ${(some [2 3 "inner" ..more] ..things)} and ${a}"})';
 
 export const ByHand = () => {
-    const map = React.useMemo(() => parseByCharacter(text).map, []);
-    const idx = (map[-1] as ListLikeContents).values[0];
+    const [state, setState] = React.useState(() => {
+        const map = parseByCharacter(initialText).map;
+        const idx = (map[-1] as ListLikeContents).values[0];
+        const at = selectEnd(
+            idx,
+            [{ idx: -1, child: { type: 'child', at: 0 } }],
+            map,
+        )!;
+        return { map, root: idx, at };
+    });
+
     const { back, sourceMap } = React.useMemo(() => {
         const sourceMap: SourceMap = { map: {}, cur: 0 };
-        const back = nodeToString(fromMCST(idx, map), sourceMap);
+        const back = nodeToString(fromMCST(state.root, state.map), sourceMap);
         return { back, sourceMap };
-    }, []);
-    const [at, setAt] = React.useState(
-        () =>
-            selectEnd(
-                idx,
-                [{ idx: -1, child: { type: 'child', at: 0 } }],
-                map,
-            )!,
-    );
-    const pos = remapPos(at.sel, sourceMap);
-    console.log(at);
+    }, [state.root, state.map]);
+
+    const pos = remapPos(state.at.sel, sourceMap);
+    console.log(state);
 
     React.useEffect(() => {
         const fn = (evt: KeyboardEvent) => {
-            setAt((at) => {
+            setState((state) => {
                 console.log('ok', evt.key);
 
                 try {
-                    const curText = idText(map[at.sel.idx]) ?? '';
-                    const pos = selPos(at.sel, curText);
+                    const curText = idText(state.map[state.at.sel.idx]) ?? '';
+                    const pos = selPos(state.at.sel, curText);
 
                     const update = getKeyUpdate(
                         evt.key,
                         pos,
                         curText,
-                        at.sel.idx,
-                        at.path,
-                        map,
+                        state.at.sel.idx,
+                        state.at.path,
+                        state.map,
                     );
-                    if (update?.type !== 'select') {
+                    if (update?.type === 'update' && update?.update) {
                         console.log('wat');
-                    } else {
+                        const map = { ...state.map };
+                        Object.keys(update.update.map).forEach((key) => {
+                            if (update.update!.map[+key] == null) {
+                                delete map[+key];
+                            } else {
+                                map[+key] = update.update!.map[+key]!;
+                            }
+                        });
+                        return {
+                            ...state,
+                            map,
+                            at: {
+                                sel: update.update.selection,
+                                path: update.update.path,
+                            },
+                        };
+                        // selection = update.update.selection;
+                        // path = update.update.path;
+                    } else if (update?.type === 'select') {
                         console.log('settings');
-                        return { sel: update.selection, path: update.path };
+                        return {
+                            ...state,
+                            at: { sel: update.selection, path: update.path },
+                        };
                     }
                 } catch (err) {
                     console.log(err);
                 }
-                return at;
+                return state;
             });
         };
         document.addEventListener('keydown', fn);
@@ -63,7 +86,7 @@ export const ByHand = () => {
     return (
         <div style={{ padding: 16 }}>
             <div style={{ position: 'relative' }}>
-                <span>{text}</span>
+                <span style={{ whiteSpace: 'pre' }}>{back}</span>
                 <div
                     style={{
                         position: 'absolute',
@@ -75,7 +98,7 @@ export const ByHand = () => {
                     }}
                 />
             </div>
-            <div>{JSON.stringify(at)}</div>
+            <div>{JSON.stringify(state.at)}</div>
         </div>
     );
 };
