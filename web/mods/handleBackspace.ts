095215e34b97bfa561147fa6cc1115acfee88003
Jared Forsyth <jared@jaredforsyth.com>
2023-03-20 08:42:09 -0500

    [nidx] some deletion

--- web/mods/handleBackspace.ts
+++ web/mods/handleBackspace.ts
@@ -7,11 +7,12 @@ import {
     MNodeExtra,
 } from '../../src/types/mcst';
 import { newBlank } from './newNodes';
-import { goLeft, selectEnd } from './navigate';
+import { goLeft, selectEnd, selectStart } from './navigate';
 import {
     StateChange,
     maybeClearParentList,
     getKeyUpdate,
+    StateSelect,
 } from './getKeyUpdate';
 import { replacePathWith } from './replacePathWith';
 import { splitGraphemes } from '../../src/parse/parse';
@@ -56,103 +57,7 @@ export function handleBackspace(
             }
         }
         if (item.type === 'nodes') {
-            const ancestor = commonAncestor(start, end);
-            if (!ancestor) {
-                console.warn('no common ancestor', start, end);
-                return;
-            }
-            const toRemove: { [idx: number]: boolean } = {};
-            item.nodes.forEach((node) => (toRemove[node.loc.idx] = true));
-
-            // hmmm changed?
-            let update: UpdateMap = {};
-
-            transformNode(fromMCST(ancestor, map), {
-                pre(node, path) {
-                    if ('values' in node) {
-                        let values = node.values.filter(
-                            (node) => !toRemove[node.loc.idx],
-                        );
-                        if (values.length < node.values.length) {
-                            update[node.loc.idx] = {
-                                ...node,
-                                values: values.map((v) => v.loc.idx),
-                            };
-                        }
-                        return;
-                    }
-                    if (node.type === 'string') {
-                        let first = node.first;
-                        if (toRemove[first.loc.idx]) {
-                            first = { ...first, text: '' };
-                        }
-                        let templates: {
-                            expr: Node;
-                            suffix: stringText & NodeExtra;
-                        }[] = [];
-                        node.templates.forEach(({ expr, suffix }) => {
-                            if (toRemove[expr.loc.idx]) {
-                                if (!toRemove[suffix.loc.idx]) {
-                                    if (templates.length) {
-                                        templates[
-                                            templates.length - 1
-                                        ].suffix.text += suffix.text;
-                                    } else {
-                                        first = {
-                                            ...first,
-                                            text: first.text + suffix.text,
-                                        };
-                                    }
-                                }
-                            } else if (toRemove[suffix.loc.idx]) {
-                                templates.push({
-                                    expr,
-                                    suffix: { ...suffix, text: '' },
-                                });
-                            } else {
-                                templates.push({ expr, suffix });
-                            }
-                        });
-                        if (first !== node.first) {
-                            update[first.loc.idx] = first;
-                        }
-                        if (templates.length !== node.templates.length) {
-                            update[node.loc.idx] = {
-                                ...node,
-                                first: first.loc.idx,
-                                templates: templates.map(({ expr, suffix }) => {
-                                    update[suffix.loc.idx] = suffix;
-                                    return {
-                                        expr: expr.loc.idx,
-                                        suffix: suffix.loc.idx,
-                                    };
-                                }),
-                            };
-                        }
-                    }
-                },
-            });
-
-            // const left = getKeyUpdate('ArrowLeft', map, { start }, () => -100);
-            let left = goLeft(start, map);
-            if (!left) {
-                console.log('cannot left');
-                return;
-            }
-            const updated = { ...map, ...update } as Map;
-            while (!validatePath(updated, left!.selection)) {
-                left = goLeft(left!.selection, map);
-                if (!left) {
-                    console.log('cannot left');
-                    return;
-                }
-            }
-
-            return {
-                type: 'update',
-                map: update,
-                selection: left.selection,
-            };
+            return removeNodes(start, end, item.nodes, map);
         }
     }
 
@@ -516,4 +421,134 @@ export const maybeRemovePrevBlank = (path: Path[], map: Map): StateChange => {
     if (gp.child.type === 'start') {
         return maybeRemovePrevBlank(path.slice(0, -1), map);
     }
+};
+
+export const removeNodes = (
+    start: Path[],
+    end: Path[],
+    nodes: Node[],
+    map: Map,
+): StateChange | void => {
+    const ancestor = commonAncestor(start, end);
+    if (!ancestor) {
+        console.warn('no common ancestor', start, end);
+        return;
+    }
+    const toRemove: { [idx: number]: boolean } = {};
+    nodes.forEach((node) => (toRemove[node.loc.idx] = true));
+
+    // hmmm changed?
+    let update: UpdateMap = {};
+
+    transformNode(fromMCST(ancestor, map), {
+        pre(node, path) {
+            if ('values' in node) {
+                let values = node.values.filter(
+                    (node) => !toRemove[node.loc.idx],
+                );
+                if (values.length < node.values.length) {
+                    update[node.loc.idx] = {
+                        ...node,
+                        values: values.map((v) => v.loc.idx),
+                    };
+                }
+                return;
+            }
+            if (node.type === 'string') {
+                let first = node.first;
+                if (toRemove[first.loc.idx]) {
+                    first = { ...first, text: '' };
+                }
+                let templates: {
+                    expr: Node;
+                    suffix: stringText & NodeExtra;
+                }[] = [];
+                node.templates.forEach(({ expr, suffix }) => {
+                    if (toRemove[expr.loc.idx]) {
+                        if (!toRemove[suffix.loc.idx]) {
+                            if (templates.length) {
+                                templates[templates.length - 1].suffix.text +=
+                                    suffix.text;
+                            } else {
+                                first = {
+                                    ...first,
+                                    text: first.text + suffix.text,
+                                };
+                            }
+                        }
+                    } else if (toRemove[suffix.loc.idx]) {
+                        templates.push({
+                            expr,
+                            suffix: { ...suffix, text: '' },
+                        });
+                    } else {
+                        templates.push({ expr, suffix });
+                    }
+                });
+                if (first !== node.first) {
+                    update[first.loc.idx] = first;
+                }
+                if (templates.length !== node.templates.length) {
+                    update[node.loc.idx] = {
+                        ...node,
+                        first: first.loc.idx,
+                        templates: templates.map(({ expr, suffix }) => {
+                            update[suffix.loc.idx] = suffix;
+                            return {
+                                expr: expr.loc.idx,
+                                suffix: suffix.loc.idx,
+                            };
+                        }),
+                    };
+                }
+            }
+        },
+    });
+
+    const updated = { ...map, ...update } as Map;
+
+    let left = start;
+
+    let last = start[start.length - 1];
+    let parent = start[start.length - 2];
+    if (parent.child.type === 'child' && parent.child.at === 0) {
+        const node = updated[parent.idx];
+        if ('values' in node) {
+            if (!node.values.length) {
+                left = start.slice(0, -2).concat({
+                    idx: parent.idx,
+                    child: { type: 'inside' },
+                });
+            } else {
+                left = selectStart(
+                    node.values[0],
+                    start.slice(0, -2).concat({
+                        idx: parent.idx,
+                        child: { type: 'child', at: 0 },
+                    }),
+                    updated,
+                )!;
+            }
+        }
+    }
+
+    // const left = getKeyUpdate('ArrowLeft', map, { start }, () => -100);
+    // let left = goLeft(start, map)?.selection;
+    // if (!left) {
+    //     console.log('cannot left');
+    //     return;
+    // }
+    while (!validatePath(updated, left!)) {
+        left = goLeft(left!, map)?.selection!;
+        if (!left) {
+            console.log('cannot left');
+            return;
+        }
+    }
+
+    return {
+        type: 'update',
+        map: update,
+        selection: left,
+    };
 };
