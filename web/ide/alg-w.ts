b9b98c4512e5fd13e2caba24f9baec6a4c133b22
Jared Forsyth <jared@jaredforsyth.com>
2023-09-25 06:53:59 -0500

    [infer-more] a little parser, as a treat

--- web/ide/alg-w.ts
+++ web/ide/alg-w.ts
@@ -1,5 +1,6 @@
 // The algo
 // based on https://jeremymikkola.com/posts/2018_03_25_understanding_algorithm_w.html
+// and https://github.com/wh5a/Algorithm-W-Step-By-Step/blob/master/AlgorithmW.lhs
 
 import { Node } from '../../src/types/cst';
 
@@ -14,26 +15,31 @@ type Exp =
     | {
           type: 'var';
           name: string;
+          loc: number;
       }
     | {
           type: 'lit';
           value: Literal;
+          loc: number;
       }
     | {
           type: 'app';
           fn: Exp;
           arg: Exp;
+          loc: number;
       }
     | {
           type: 'fn';
           name: string;
           body: Exp;
+          loc: number;
       }
     | {
           type: 'let';
           name: string;
           init: Exp;
           body: Exp;
+          loc: number;
       };
 
 type Type =
@@ -50,6 +56,82 @@ type Type =
           arg: Type;
           body: Type;
       };
+
+export const parse = (
+    node: Node,
+    errors: { [key: number]: string },
+): Exp | undefined => {
+    switch (node.type) {
+        case 'identifier': {
+            const num = +node.text;
+            if (!isNaN(num)) {
+                return {
+                    type: 'lit',
+                    value: { type: 'number', value: num },
+                    loc: node.loc,
+                };
+            }
+            if (node.text === 'true' || node.text === 'false') {
+                return {
+                    type: 'lit',
+                    value: { type: 'bool', value: node.text === 'true' },
+                    loc: node.loc,
+                };
+            }
+            return { type: 'var', name: node.text, loc: node.loc };
+        }
+        case 'list': {
+            if (
+                node.values.length === 3 &&
+                node.values[0].type === 'identifier' &&
+                node.values[0].text === 'fn' &&
+                node.values[1].type === 'array' &&
+                node.values[1].values.length === 1 &&
+                node.values[1].values[0].type === 'identifier'
+            ) {
+                const body = parse(node.values[2], errors);
+                if (body == null) return;
+                return {
+                    type: 'fn',
+                    name: node.values[1].values[0].text,
+                    loc: node.loc,
+                    body,
+                };
+            }
+
+            if (node.values.length === 2) {
+                const fn = parse(node.values[0], errors);
+                const arg = parse(node.values[1], errors);
+                return fn && arg
+                    ? { type: 'app', fn, arg, loc: node.loc }
+                    : undefined;
+            }
+
+            if (
+                node.values.length === 3 &&
+                node.values[0].type === 'identifier' &&
+                node.values[0].text === 'let' &&
+                node.values[1].type === 'array' &&
+                node.values[1].values.length === 2 &&
+                node.values[1].values[0].type === 'identifier'
+            ) {
+                const name = node.values[1].values[0].text;
+                const init = parse(node.values[1].values[1], errors);
+                const body = parse(node.values[2], errors);
+
+                return init && body
+                    ? {
+                          type: 'let',
+                          body,
+                          init,
+                          name,
+                          loc: node.loc,
+                      }
+                    : undefined;
+            }
+        }
+    }
+};
 
 type Scheme = {
     vbls: string[];
