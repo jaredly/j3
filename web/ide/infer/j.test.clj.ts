0c017076b552933f0f59f52fa2ea0db43891b30e
Jared Forsyth <jared@jaredforsyth.com>
2023-09-26 06:59:16 -0500

    [infer-more] ok tests actually working

--- web/ide/infer/j.test.clj.ts
+++ web/ide/infer/j.test.clj.ts
@@ -21,4 +21,21 @@ x different arg numbers
 (let [id (fn [x] x)]
     ((fn [a b] a) (id 2) (id "hi")))
 -> number
+
+(let [x 10] x)
+
+(+ true 1)
+
+; non-polymorphic fn args
+(fn [id] ((fn [a b] a) (id 10) (id "hi")))
+
+(if true 1 2)
+
+(fn [x y] (if true x y))
+
+(fn [x] (if true x 2))
+
+(fn [x y] (if true x 2))
+
+(let [id (fn [x] x)] (if true (id 2) (let [m (id "hi")] 10)))
 `;
