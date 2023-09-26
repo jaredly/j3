3f4d25cfbb7b45f076fcf1e84997af5a96dd424c
Jared Forsyth <jared@jaredforsyth.com>
2023-09-26 07:00:50 -0500

    [infer-more] some more tests

--- web/ide/infer/j.test.clj.ts
+++ web/ide/infer/j.test.clj.ts
@@ -23,19 +23,35 @@ x different arg numbers
 -> number
 
 (let [x 10] x)
+-> number
 
 (+ true 1)
+x cannot unify number and bool
 
 ; non-polymorphic fn args
 (fn [id] ((fn [a b] a) (id 10) (id "hi")))
+x cannot unify number and string
 
 (if true 1 2)
+-> number
 
 (fn [x y] (if true x y))
+-> (fn [v2:1 v2:1] v2:1)
 
 (fn [x] (if true x 2))
+-> (fn [number] number)
 
 (fn [x y] (if true x 2))
+-> (fn [number v2:1] number)
 
-(let [id (fn [x] x)] (if true (id 2) (let [m (id "hi")] 10)))
+(let [id (fn [x] x)]
+    (if true
+        (id 2)
+        (let [m (id "hi")] 10)))
+-> number
+
+(let [x 1]
+    (let [y x]
+        10))
+-> number
 `;
