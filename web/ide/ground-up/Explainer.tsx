aa47b59346356109c691e46d5991e26d39a7ea46
Jared Forsyth <jared@jaredforsyth.com>
2024-05-09 21:37:07 -0500

    [explainers] bounce

---
+++ web/ide/ground-up/Explainer.tsx
@@ -0,0 +1,7 @@
+/**
+ * Ok this is our root class for explainers.
+ * Some things about explainers:
+ * - the files list is fixed
+ * - we set things up so you can use a server, or alternatively localforage
+ * - links ... I think we want `?` query strings instead of hashes. `pushstate` right?
+ */
