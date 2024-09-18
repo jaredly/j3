5c1f9757b8e4f9cebf495fedb1eb7111c5a1aa64
Jared Forsyth <jared@jaredforsyth.com>
2024-09-18 06:37:04 -0500

    [worker] btt

--- one-world/shared/IR/block-to-attributed-text.ts
+++ one-world/shared/IR/block-to-attributed-text.ts
@@ -58,11 +58,11 @@ const mergeStyle = (one?: Style, two?: Style) => {
     return { ...one, ...two };
 };
 
-export { blockToText } from './block-to-text';
-// export const blockToText = (pos: BlockPos, block: Block, ctx: BlockCtx) => {
-//     const ablock = blockToABlock(pos, block, ctx);
-//     return aBlockToString(ablock, ctx.color);
-// };
+// export { blockToText } from './block-to-text';
+export const blockToText = (pos: BlockPos, block: Block, ctx: BlockCtx) => {
+    const ablock = blockToABlock(pos, block, ctx);
+    return aBlockToString(ablock, ctx.color);
+};
 
 export const blockFormat = (text: ABlock, style?: Style) => {
     if (!style) return text;
