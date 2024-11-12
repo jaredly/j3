15b79cac12f9eea5e046748ca660debb31f5b8d8
Jared Forsyth <jared@jaredforsyth.com>
2024-11-12 07:44:48 -0600

    [jsss] little refactors

--- one-world/keyboard/splitInList.ts
+++ one-world/keyboard/splitInList.ts
@@ -1,4 +1,4 @@
-import { Id } from '../shared/cnodes';
+import { Id, List } from '../shared/cnodes';
 import { addBlankAfter } from './addBlankAfter';
 import {
     IdCursor,
@@ -40,52 +40,22 @@ export function splitInList(
         if (gnode.type === 'list' && gnode.kind === 'smooshed') {
             throw new Error('double smoosked');
         }
-        const pat = pnode.children.indexOf(id.loc);
-        if (pat === -1) throw new Error('not in parent');
-        const cleft = pnode.children.slice(0, pat + 1);
-        const cright = pnode.children.slice(pat + 1);
-        let nextLoc = top.nextLoc;
-        const rloc = nextLoc++;
-        cright.unshift(rloc);
 
-        const nodes: Update['nodes'] = {
-            [id.loc]: { ...id, text: left.join('') },
-            [rloc]: { type: 'id', text: right.join(''), loc: rloc },
-        };
-
-        const replace: number[] = [];
-        if (cleft.length === 1) {
-            replace.push(id.loc);
-            nodes[pnode.loc] = null;
-        } else {
-            replace.push(ploc);
-            nodes[pnode.loc] = { ...pnode, children: cleft };
-        }
-
-        if (cright.length > 1) {
-            if (cleft.length === 1) {
-                nodes[pnode.loc] = { ...pnode, children: cright };
-                replace.push(ploc);
-            } else {
-                const rploc = nextLoc++;
-                nodes[rploc] = {
-                    type: 'list',
-                    kind: 'smooshed',
-                    children: cright,
-                    loc: rploc,
-                };
-                replace.push(rploc);
-            }
-        } else {
-            replace.push(rloc);
-        }
+        const { nodes, replace, nextLoc } = smooshSplit(
+            pnode,
+            top,
+            id,
+            left,
+            right,
+            ploc,
+        );
 
         return {
             nodes: {
                 ...nodes,
                 ...replaceWithListItems(
                     path.children.slice(0, -1),
-                    { ...top, nextLoc: top.nextLoc },
+                    top,
                     ploc,
                     replace,
                 ),
@@ -116,4 +86,55 @@ export function splitInList(
         },
         nextLoc,
     };
+}
+
+function smooshSplit(
+    pnode: List<number>,
+    top: Top,
+    id: Id<number>,
+    left: string[],
+    right: string[],
+    ploc: number,
+) {
+    const pat = pnode.children.indexOf(id.loc);
+    if (pat === -1) throw new Error('not in parent');
+
+    const cleft = pnode.children.slice(0, pat + 1);
+    const cright = pnode.children.slice(pat + 1);
+    let nextLoc = top.nextLoc;
+    const rloc = nextLoc++;
+    cright.unshift(rloc);
+
+    const nodes: Update['nodes'] = {
+        [id.loc]: { ...id, text: left.join('') },
+        [rloc]: { type: 'id', text: right.join(''), loc: rloc },
+    };
+
+    const replace: number[] = [];
+    if (cleft.length === 1) {
+        replace.push(id.loc);
+        nodes[pnode.loc] = null;
+    } else {
+        replace.push(ploc);
+        nodes[pnode.loc] = { ...pnode, children: cleft };
+    }
+
+    if (cright.length > 1) {
+        if (cleft.length === 1) {
+            nodes[pnode.loc] = { ...pnode, children: cright };
+            replace.push(ploc);
+        } else {
+            const rploc = nextLoc++;
+            nodes[rploc] = {
+                type: 'list',
+                kind: 'smooshed',
+                children: cright,
+                loc: rploc,
+            };
+            replace.push(rploc);
+        }
+    } else {
+        replace.push(rloc);
+    }
+    return { nodes, replace, nextLoc };
 }
