da99722aef2e58224a9a991ebf871b0d6eb27c3d
Jared Forsyth <jared@jaredforsyth.com>
2024-11-25 07:07:42 -0600

    [ccls] parrsing prefixes and smooshse better now

--- one-world/syntaxes/gleam2.ts
+++ one-world/syntaxes/gleam2.ts
@@ -59,10 +59,14 @@ type Expr =
     | { type: 'array'; items: SExpr[]; src: Src }
     | { type: 'tuple'; items: SExpr[]; src: Src }
     | { type: 'bops'; left: Expr; rights: { op: Id<Loc>; right: Expr }[]; src: Src }
-    | ESmoosh
+    // | ESmoosh
     | { type: 'record'; rows: RecordRow[]; src: Src }
+    | { type: 'call'; target: Expr; args: SExpr[]; src: Src }
+    | { type: 'uop'; op: Id<Loc>; target: Expr; src: Src }
+    | { type: 'attribute'; target: Expr; attribute: Id<Loc>; src: Src }
+    | { type: 'index'; target: Expr; items: SExpr[]; src: Src }
     | Fancy;
-type ESmoosh = { type: 'smooshed'; prefixes: Id<Loc>[]; base: Expr; suffixes: Suffix[]; src: Src };
+// type ESmoosh = { type: 'smooshed'; prefixes: Id<Loc>[]; base: Expr; suffixes: Suffix[]; src: Src };
 
 type Suffix =
     | { type: 'index'; items: SExpr[]; src: Src }
@@ -80,7 +84,6 @@ const aToXML = (v: any, name?: string): XML | XML[] | null => {
         return null;
     }
     if (!v || typeof v !== 'object') {
-        //  || !v.type || !v.src
         return null;
     }
     if (Object.keys(v).length === 0) return null;
@@ -97,6 +100,7 @@ const aToXML = (v: any, name?: string): XML | XML[] | null => {
     if (Object.keys(children).length) res.children = children;
     return res;
 };
+
 export const toXML = (v: any) => {
     const res = aToXML(v);
     if (!res || Array.isArray(res)) {
@@ -106,184 +110,61 @@ export const toXML = (v: any) => {
     return res;
 };
 
-// const patToXML = (pat: SPat): XML => {
-//     switch (pat.type) {
-//         case 'bound':
-//             return { tag: pat.type, src: pat.src, attrs: { name: pat.name } };
-//         case 'array':
-//             return { tag: pat.type, src: pat.src, children: { values: pat.values.map(patToXML) } };
-//         case 'constr':
-//             return { tag: pat.type, src: pat.src, attrs: { constr: pat.constr }, children: { args: pat.args.map(patToXML) } };
-//         case 'text':
-//             return { tag: pat.type, src: pat.src, children: { spans: pat.spans.map((span) => textSpanToXML(span, patToXML)) } };
-//         case 'spread':
-//             return { tag: pat.type, src: pat.src, children: { inner: patToXML(pat.inner) } };
-//         default:
-//             throw new Error(`Unknown pattern type: ${(pat as any).type}`);
-//     }
-// };
-
-// const suffixToXML = (suffix: Suffix): XML => {
-//     switch (suffix.type) {
-//         case 'index':
-//             return { tag: 'index', children: { items: suffix.items.map(toXML) }, src: suffix.src };
-//         case 'call':
-//             return { tag: 'call', children: { items: suffix.items.map(toXML) }, src: suffix.src };
-//         case 'attribute':
-//             return { tag: 'attribute', attrs: { attribute: suffix.attribute }, src: suffix.src };
-//         default:
-//             throw new Error(`Unknown suffix type: ${(suffix as any).type}`);
-//     }
-// };
-
-// export const toXML = (value: SExpr | Stmt): XML => {
-//     switch (value.type) {
-//         case 'let':
-//             return { tag: 'let', src: value.src, children: { pat: patToXML(value.pat), value: toXML(value.value) } };
-//         case 'expr':
-//             return { tag: 'stmt-expr', src: value.src, children: { expr: toXML(value.expr) } };
-//         case 'local':
-//             return { tag: 'local', src: value.src, attrs: { name: value.name } };
-//         case 'global':
-//             return { tag: 'global', src: value.src, attrs: { id: value.id } };
-//         case 'text':
-//             return { tag: 'text', src: value.src, children: { spans: value.spans.map((span) => textSpanToXML(span, toXML)) } };
-//         case 'array':
-//             return { tag: 'array', src: value.src, children: { items: value.items.map(toXML) } };
-//         case 'tuple':
-//             return { tag: 'tuple', src: value.src, children: { items: value.items.map(toXML) } };
-//         case 'bops':
-//             return {
-//                 tag: 'bops',
-//                 src: value.src,
-//                 children: {
-//                     left: toXML(value.left),
-//                     rights: value.rights.map((right) => ({
-//                         tag: 'right',
-//                         src: { left: right.op.loc, right: right.right.src.right ?? right.right.src.left },
-//                         attrs: { op: right.op.text },
-//                         children: { expr: toXML(right.right) },
-//                     })),
-//                 },
-//             };
-//         case 'smooshed':
-//             return {
-//                 tag: 'smooshed',
-//                 src: value.src,
-//                 children: {
-//                     prefixes: value.prefixes.map((prefix) => ({ tag: 'prefix', attrs: { id: prefix }, src: { left: prefix.loc } })),
-//                     base: toXML(value.base),
-//                     suffixes: value.suffixes.map(suffixToXML),
-//                 },
-//             };
-//         case 'record':
-//             return {
-//                 tag: 'record',
-//                 src: value.src,
-//                 children: {
-//                     rows: value.rows.map(
-//                         (row): XML =>
-//                             row.type === 'single'
-//                                 ? toXML(row.inner)
-//                                 : {
-//                                       tag: 'row',
-//                                       attrs: { key: row.key },
-//                                       src: { left: row.key.loc, right: row.value.src.right ?? row.value.src.left },
-//                                       children: {
-//                                           value: toXML(row.value),
-//                                       },
-//                                   },
-//                     ),
-//                 },
-//             };
-//         case 'spread':
-//             return { tag: 'spread', src: value.src, children: { inner: toXML(value.inner) } };
-//         case 'if':
-//             return {
-//                 tag: value.type,
-//                 src: value.src,
-//                 children: {
-//                     cond: toXML(value.cond),
-//                     yes: value.yes.map(toXML),
-//                     no: value.no?.map(toXML),
-//                 },
-//             };
-//         case 'case':
-//             return {
-//                 tag: value.type,
-//                 src: value.src,
-//                 children: {
-//                     cases: value.cases.map(({ pat, body }) => ({
-//                         tag: 'case',
-//                         children: {
-//                             pat: patToXML(pat),
-//                             body: toXML(body),
-//                         },
-//                         src: { left: [] },
-//                     })),
-//                 },
-//             };
-
-//         default:
-//             throw new Error(`Unknown node type: ${(value as any).type}`);
-//     }
-// };
-
 const binned_ = mref<Expr>('binned');
 
 type Fancy =
     | { type: 'if'; cond: Expr; yes: Stmt[]; no?: Stmt[]; src: Src }
     | { type: 'case'; target: Expr; cases: { pat: Pat; body: Expr }[]; src: Src };
 
-export type Visitor = {
-    stmt: (s: Stmt) => void;
-    expr: (e: Expr) => void;
-    pat: (p: Pat) => void;
-    sexpr: (e: SExpr) => void;
-};
+// export type Visitor = {
+//     stmt: (s: Stmt) => void;
+//     expr: (e: Expr) => void;
+//     pat: (p: Pat) => void;
+//     sexpr: (e: SExpr) => void;
+// };
 
-export const visitSExpr = (expr: SExpr, v: Visitor) => {
-    if (expr.type === 'spread') {
-        v.sexpr(expr);
-        visitExpr(expr.inner, v);
-    } else {
-        visitExpr(expr, v);
-    }
-};
+// export const visitSExpr = (expr: SExpr, v: Visitor) => {
+//     if (expr.type === 'spread') {
+//         v.sexpr(expr);
+//         visitExpr(expr.inner, v);
+//     } else {
+//         visitExpr(expr, v);
+//     }
+// };
 
-export const visitExpr = (expr: Expr, v: Visitor) => {
-    v.expr(expr);
-    switch (expr.type) {
-        case 'array':
-        case 'tuple':
-            expr.items.forEach((child) => visitSExpr(child, v));
-            break;
-        case 'if':
-            visitExpr(expr.cond, v);
-            expr.yes.forEach((s) => visitStmt(s, v));
-            expr.no?.forEach((s) => visitStmt(s, v));
-            break;
-        case 'local':
-        case 'global':
-            return;
-    }
-};
+// export const visitExpr = (expr: Expr, v: Visitor) => {
+//     v.expr(expr);
+//     switch (expr.type) {
+//         case 'array':
+//         case 'tuple':
+//             expr.items.forEach((child) => visitSExpr(child, v));
+//             break;
+//         case 'if':
+//             visitExpr(expr.cond, v);
+//             expr.yes.forEach((s) => visitStmt(s, v));
+//             expr.no?.forEach((s) => visitStmt(s, v));
+//             break;
+//         case 'local':
+//         case 'global':
+//             return;
+//     }
+// };
 
-export const visitPat = (pat: Pat, v: Visitor) => {
-    v.pat(pat);
-};
+// export const visitPat = (pat: Pat, v: Visitor) => {
+//     v.pat(pat);
+// };
 
-export const visitStmt = (stmt: Stmt, v: Visitor) => {
-    v.stmt(stmt);
-    switch (stmt.type) {
-        case 'expr':
-            return visitExpr(stmt.expr, v);
-        case 'let':
-            visitPat(stmt.pat, v);
-            visitExpr(stmt.value, v);
-            return;
-    }
-};
+// export const visitStmt = (stmt: Stmt, v: Visitor) => {
+//     v.stmt(stmt);
+//     switch (stmt.type) {
+//         case 'expr':
+//             return visitExpr(stmt.expr, v);
+//         case 'let':
+//             visitPat(stmt.pat, v);
+//             visitExpr(stmt.value, v);
+//             return;
+//     }
+// };
 
 const fancy = switch_<Expr, Expr>(
     [
@@ -341,7 +222,7 @@ const _exmoosh: Matcher<Omit<ESmoosh, 'type'>>[] = [
         multi(
             switch_(
                 [
-                    sequence<Suffix>([kwd('.', () => ({ type: 'attribute' })), named('attr', id('attribute', idt))], false, idt),
+                    sequence<Suffix>([kwd('.', () => ({ type: 'attribute' })), named('attribute', id('attribute', idt))], false, idt),
                     list<0, SExpr[], Suffix>('square', multi(sprexpr_, true), (items, node) => ({ type: 'index', items, src: nodesSrc(node) })),
                     list<0, SExpr[], Suffix>('round', multi(sprexpr_, true), (items, node) => ({ type: 'call', items, src: nodesSrc(node) })),
                 ],
@@ -352,11 +233,44 @@ const _exmoosh: Matcher<Omit<ESmoosh, 'type'>>[] = [
     ),
 ];
 
-const _nosexpr: Matcher<Expr> = list('smooshed', sequence(_exmoosh, true, idt), (data, node) => ({
-    type: 'smooshed',
-    ...data,
-    src: nodesSrc(node),
-}));
+type Smoosh = {
+    base: Expr;
+    suffixes: Suffix[];
+    prefixes: Id<Loc>[];
+};
+
+const parseSmoosh = ({ base, suffixes, prefixes }: Smoosh, node: RecNode): Expr => {
+    suffixes.forEach((suffix) => {
+        switch (suffix.type) {
+            case 'attribute':
+                base = { type: 'attribute', target: base, attribute: suffix.attribute, src: mergeSrc(base.src, suffix.src) };
+                return;
+            case 'call':
+                base = { type: 'call', target: base, args: suffix.items, src: mergeSrc(base.src, suffix.src) };
+                return;
+            case 'index':
+                base = { type: 'index', target: base, items: suffix.items, src: mergeSrc(base.src, suffix.src) };
+                return;
+        }
+    });
+    for (let i = prefixes.length - 1; i >= 0; i--) {
+        base = { type: 'uop', op: prefixes[i], target: base, src: mergeSrc(nodesSrc(prefixes[i]), base.src) };
+    }
+    return base;
+};
+
+const mergeSrc = (one: Src, two?: Src): Src => ({ left: one.left, right: two?.right ?? two?.left ?? one.right });
+
+const _nosexpr: Matcher<Expr> = list(
+    'smooshed',
+    sequence<Smoosh>(_exmoosh, true, idt),
+    (data, node) => parseSmoosh(data, node),
+    //     {
+    //     type: 'smooshed',
+    //     ...data,
+    //     src: nodesSrc(node),
+    // }
+);
 
 const _sprood: Matcher<SExpr> = list(
     'smooshed',
