908a46b7fdbeb58fe03c8a6f02f654a1f4110339
Jared Forsyth <jared@jaredforsyth.com>
2025-01-07 22:06:47 -0600

    [use-selstat] hm mid should be statuses I believe

--- one-world/keyboard/selections.test.ts
+++ one-world/keyboard/selections.test.ts
@@ -61,7 +61,7 @@ test('list multi', () => {
     });
 });
 
-test.only('list into', () => {
+test('list into', () => {
     let state = asTop(round([id('hi', 1), round([id('ha'), id('ho', 2)])]), idc(1), idc(1));
     const statuses = getSelectionStatuses(state.sel, state.top);
     expect(statuses).toEqual({
@@ -74,5 +74,76 @@ test.only('list into', () => {
             cursors: [idc(1)],
             highlight: { type: 'id', end: 1 },
         },
+    });
+});
+
+test('text simple', () => {
+    let state = asTop(text([tspan('before'), tspan('after')], 1), textc(0, 2), textc(0, 4));
+    const statuses = getSelectionStatuses(state.sel, state.top);
+    expect(statuses).toEqual({
+        ';;0': {
+            cursors: [textc(0, 2), textc(0, 4)],
+            highlight: { type: 'text', spans: [{ start: 2, end: 4 }, false] },
+        },
+    });
+});
+
+test('text simple across spans', () => {
+    let state = asTop(text([tspan('before'), tspan('cover'), tspan('after')], 1), textc(0, 2), textc(2, 2));
+    const statuses = getSelectionStatuses(state.sel, state.top);
+    expect(statuses).toEqual({
+        ';;0': {
+            cursors: [textc(0, 2), textc(2, 2)],
+            highlight: { type: 'text', spans: [{ start: 2 }, true, { end: 2 }] },
+        },
+    });
+});
+
+test('text into', () => {
+    let state = asTop(
+        text([tspan('before'), { type: 'embed', item: round([id('over'), id('inner', 2), id('yall')]) }, tspan('after')], 1),
+        textc(0, 2),
+        idc(2),
+    );
+    const statuses = getSelectionStatuses(state.sel, state.top);
+    expect(statuses).toEqual({
+        ';;0': {
+            cursors: [textc(0, 2)],
+            highlight: {
+                type: 'text',
+                spans: [{ start: 2 }, false, false],
+            },
+        },
+        // ';;0,1': {
+        //     cursors: [],
+        //     highlight: { type: 'list', opener: true, closer: false },
+        // },
+        ';;0,1,2': { cursors: [], highlight: { type: 'full' } },
+        ';;0,1,3': { cursors: [idc(2)], highlight: { type: 'id', end: 2 } },
+    });
+});
+
+test('text into more', () => {
+    let state = asTop(
+        round([id('abcd', 1), text([tspan('before'), { type: 'embed', item: round([id('over'), id('inner', 2), id('yall')]) }, tspan('after')])]),
+        idc(3),
+        idc(2),
+    );
+    const statuses = getSelectionStatuses(state.sel, state.top);
+    expect(statuses).toEqual({
+        ';;0,1': {
+            cursors: [idc(3)],
+            highlight: { type: 'id', start: 3 },
+        },
+        ';;0,2': {
+            cursors: [],
+            highlight: { type: 'text', spans: [true, false, false] },
+        },
+        // ';;0,2,3': {
+        //     cursors: [],
+        //     highlight: { type: 'list', opener: true, closer: false },
+        // },
+        ';;0,2,3,4': { cursors: [], highlight: { type: 'full' } },
+        ';;0,2,3,5': { cursors: [idc(2)], highlight: { type: 'id', end: 2 } },
     });
 });
